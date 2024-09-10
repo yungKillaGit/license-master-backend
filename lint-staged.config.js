4abedbc12aa509b83d07c411a981843c994234e0
Artur Iskhakov <artur.iskhakov@waveaccess.global>
2024-09-10 23:18:33 +0500

    Setup eslint-kit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{js,ts}': ['eslint --fix', 'eslint'],
-    '*.{json,yaml}': ['prettier --write'],
+  '*.{js,ts}': ['eslint --fix', 'eslint'],
+  '*.{json,yaml}': ['prettier --write'],
 };
